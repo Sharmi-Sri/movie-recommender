7448d0da88a88187b577d2d74a8c4b9016421cfa
Sharmi <alonegirlsharmi@gmail.com>
2026-09-04 16:58:41 +0530

    Add deployment config for Render

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def home():
                             sort_by=sort_by)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
